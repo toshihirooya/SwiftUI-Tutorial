196f5bbbae36589f80629d4186a8fac44d3f9829
toshihirooya <contact@toshihirooya.com>
2020-01-22 10:59:12 +0900

    section 6

--- Landmarks/MapView.swift
+++ Landmarks/MapView.swift
@@ -13,7 +13,7 @@ struct MapView: UIViewRepresentable {
     func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
     }
-    
+
     func updateUIView(_ view: MKMapView, context: Context) {
         let coordinate = CLLocationCoordinate2D(
             latitude: 34.011286, longitude: -116.166868)
@@ -23,7 +23,7 @@ struct MapView: UIViewRepresentable {
     }
 }
 
-struct MapView_Previews: PreviewProvider {
+struct MapView_Preview: PreviewProvider {
     static var previews: some View {
         MapView()
     }
